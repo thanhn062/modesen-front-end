b5edb131c6e3e31caa76fe02554af39f3033933f
yinliyuan <1664834632@qq.com>
2018-11-16 19:48:43 +0800

    signup

--- modesens/pages/accounts/signup.vue
+++ modesens/pages/accounts/signup.vue
@@ -2,7 +2,7 @@
   <section>
     <h2>创建账号</h2>
     <div>请输入以下信息创建你的账户</div>
-    <b-form>
+    <b-form @submit="login">
       <b-form-input
         v-model="email"
         type="email"
@@ -19,21 +19,39 @@
           v-model="selected"
           :options="options"/>
       </b-form-group>
-      <b-button>注册</b-button>
+      <b-button type="submit">注册</b-button>
     </b-form>
   </section>
 </template>
 <script>
+import account from '~/static/api/1.0/account.js'
 export default {
   data() {
     return {
       email: '',
       password: '',
-      selected: 'first',
-      options: [
-        { text: '女士', value: 'first' },
-        { text: '男士', value: 'second' }
-      ]
+      selected: 'f',
+      options: [{ text: '女士', value: 'f' }, { text: '男士', value: 'm' }]
+    }
+  },
+  methods: {
+    async login(evt) {
+      evt.preventDefault()
+      let data = new Object()
+      data.client_id = this.$auth.options.client_id
+      data.client_secret = this.$auth.options.client_secret
+      data.grant_type = 'password'
+      data.username = 'MSRG-' + this.email
+      data.password = this.password
+      data.gender = this.selected
+      try {
+        let {
+          data: { access_token, token_type }
+        } = await account.login(data)
+        alert('注册成功')
+      } catch (e) {
+        this.error = e.message
+      }
     }
   }
 }
