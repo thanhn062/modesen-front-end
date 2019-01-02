2603e40558ec0405f7fcd04d3161df9f179fa602
Thanh Ngo <510thanh.ngo@gmail.com>
2019-01-02 17:52:16 +0800

    fix

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ cd /modesens/modesens-frontend/modesens
 rm -rf .nuxt/*
 npm install
 aws s3 sync s3://modesens/nuxt .nuxt
-pm2 startOrReload server.json --update-env
+pm2 startOrReload server.json -u root --update-env
 
