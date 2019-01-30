ff3c4481ccfaa25b84b12007812ddca40cb43cc3
BaoXin <bx1051814980@163.com>
2019-01-30 12:20:23 +0800

    bx

--- modesens/components/Modals.vue
+++ modesens/components/Modals.vue
@@ -48,8 +48,8 @@
           <br>
           <div>
             {{ $t('cuModal.desc2') }}<br>
-            <span v-if="$i18n.locale==='zh'">访问我们的<a href="/faq/">帮助中心</a>，在那里你可以找到常见问题解决方案，以及客服中心联络方式信息。</span>
-            <span v-else>Visit our <a href="/faq/">Help Center</a> where you can find frequently asked questions, as well as Customer Care contact information.</span>
+            <span v-if="$i18n.locale==='zh'">访问我们的<a href="/faq/" target="_self">帮助中心</a>，在那里你可以找到常见问题解决方案，以及客服中心联络方式信息。</span>
+            <span v-else>Visit our <a href="/faq/" target="_self">Help Center</a> where you can find frequently asked questions, as well as Customer Care contact information.</span>
           </div>
           <br>
           <div>
