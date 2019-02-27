b00e2f41fb5eb42c10952dd41c9546a576f66997
yinliyuan <1664834632@qq.com>
2019-02-27 14:24:01 +0800

    rename

--- modesens/pages/account/more.vue
+++ modesens/pages/account/more.vue
@@ -45,7 +45,7 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-@import '../assets/css/common.less';
+@import '../../assets/css/common.less';
 .more-container {
   .menulist {
     padding: 0 30px;
