e0cf7bcbd90d01cfcb7745e6a0a77bc3ed58f1c0
BaoXin <bx1051814980@163.com>
2019-03-19 14:08:26 +0800

    meta

--- modesens/nuxt.config.js
+++ modesens/nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      // { hid: 'description', name: 'description', content: pkg.description },
       { name: 'theme-color', content: '#FFF' },
       { name: 'ir-site-verification-token', value: '-1470320165' },
       { property: 'og:type', content: 'website' },
