401492721d0dc9e1000f74c158fa22250fdcb6e9
BaoXin <bx1051814980@163.com>
2019-04-01 16:13:34 +0800

    bx

--- modesens/layouts/default.vue
+++ modesens/layouts/default.vue
@@ -382,6 +382,24 @@ export default {
   },
   created() {},
   mounted() {
+    // sw
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(
+          function(registration) {
+            // Registration was successful
+            console.log(
+              'ServiceWorker registration successful with scope: ',
+              registration.scope
+            )
+          },
+          function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err)
+          }
+        )
+      })
+    }
     if (!this.$store.state.request) {
       this.$store.dispatch('getRequest', this.$axios)
     }
