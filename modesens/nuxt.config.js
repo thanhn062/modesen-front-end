4d230bf664164c0664dbd5a00c125bf85847b8aa
Thanh Ngo <510thanh.ngo@gmail.com>
2019-04-18 19:53:23 +0800

    fix

--- modesens/nuxt.config.js
+++ modesens/nuxt.config.js
@@ -157,6 +157,7 @@ module.exports = {
 	** Build configuration
 	*/
   build: {
+    // extractCSS: { allChunks: true },
     publicPath: 'https://cdn.modesens.com',
     optimization: {
       splitChunks: {
